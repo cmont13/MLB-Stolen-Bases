a88de40b1b5223fee7023bde6d7a0d502549e5f0
cmont13 <130933332+cmont13@users.noreply.github.com>
2023-04-16 17:09:52 -0400

    Update Make_Poster.R

--- Make_Poster.R
+++ Make_Poster.R
@@ -1,5 +1,5 @@
 # Load packages####
-install.packages(c('tidyverse', 'readxl', 'png', 'jpg', 'svDialogs', 'magick'))
+#install.packages(c('tidyverse', 'readxl', 'png', 'jpg', 'svDialogs', 'magick'))
 library(tidyverse)  # Loads the `tidyverse` collection
 library(readxl)     # Reads CSV and Excel files
 library(png)        # Reads and Writes PNG files
@@ -8,7 +8,7 @@ library(svDialogs)  # Generates pop-up dialogue
 library(magick)
 
 #Read Data####
-urls <- read.csv("https://github.com/cmont13/MLB-Stolen-Bases/blob/main/Data/urls2.csv")
+urls <- read.csv("MLB-Stolen-Bases/Data/urls2.csv")
 
 
 #Take input from user####
@@ -19,10 +19,10 @@ sheriff <- dlgInput("Enter a team name", Sys.info()["user"])$res |>
   toupper()
 sheriff <- paste("THE", sheriff)
 count <- dlgInput("Enter a number", Sys.info()["user"])$res
-offenses <-image_read("https://github.com/cmont13/MLB-Stolen-Bases/blob/main/Images/First_Offense_Tag.png")|>
+offenses <-image_read("MLB-Stolen-Bases/Images/First_Offense_Tag.png")|>
   image_scale('25%')
 if(count > 1) {
-  offenses <-image_read("https://github.com/cmont13/MLB-Stolen-Bases/blob/main/Images/Repeat_Offender_Tag.png")|>
+  offenses <-image_read("MLB-Stolen-Bases/Images/Repeat_Offender_Tag.png")|>
     image_scale('25%')
 }
   
@@ -42,7 +42,7 @@ player_url <- temp_url |>
   toString()
 
 #Generating Image####
-destfile <- "https://github.com/cmont13/MLB-Stolen-Bases/blob/main/Images/output.png"
+destfile <- "MLB-Stolen-Bases/output.png"
 temp <- tempfile()
 download.file(player_url, temp, mode = "wb")
 headshot <- readPNG(temp)
@@ -51,16 +51,16 @@ writePNG(headshot, destfile)
 #file.remove(temp)
 
 #Draw Poster####
-photo <- image_read("https://github.com/cmont13/MLB-Stolen-Bases/blob/main/Images/output.png") |>
+photo <- image_read("MLB-Stolen-Bases/output.png") |>
   image_scale("480x349") |>
   image_quantize(colorspace = 'gray')|>
   image_enhance() |>
   image_colorize(40, "#C7791A")
-poster <- image_read("https://github.com/cmont13/MLB-Stolen-Bases/blob/main/Images/Blank_Wanted_template.jpg")
+poster <- image_read("MLB-Stolen-Bases/Blank_Wanted_template.jpg")
 poster <- image_scale(poster, '25%')
 poster <- poster |>
   image_annotate(sheriff, font = 'Georgia', size = 35, location = "+140+192", color = "#403A3A", weight = 550) |>
   image_composite(composite_image = photo, offset = "+50+293")|>
   image_composite(composite_image = offenses)
 poster
-image_write(poster, path = "https://github.com/cmont13/MLB-Stolen-Bases/blob/main/Images/final_poster.jpeg", format = "jpeg")
+image_write(poster, path = "MLB-Stolen-Bases/Images/final_poster.jpeg", format = "jpeg")
